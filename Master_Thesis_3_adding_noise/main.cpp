8b24e96c3a9c0e53fc8e0c9241ec9041d6e3935e
Iwan Phillips <iwanguitar@hotmail.co.uk>
2020-11-27 16:31:47 +0100

    Initial Commit

---
+++ Master_Thesis_3_adding_noise/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Master_Thesis_3_adding_noise
+//
+//  Created by Iwan Phillips on 27.11.20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
